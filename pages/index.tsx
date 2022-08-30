aa01707d080a08a77e6a71bbb40024622b4ab7da
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-08-30 21:45:08 +0200

    UP add form, validade, send and get context

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,34 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { FormEvent, useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useContext(AuthContext); //Iniciar o contexto para obter as informações
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const data = { email, password };
+    await signIn(data);
+  }
+
   return (
-    <div className={styles.container}>
-      <h1>Hello World!</h1>
-    </div>
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Entrar</button>
+    </form>
   );
 };
 
