1f0acfe2e7d7330027578dcafb1a622bd97186a9
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-08-31 22:41:13 +0200

    UP read and delete cookies, singIn and signOut fun

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import Router from "next/router";
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies, destroyCookie } from "nookies";
 import { api } from "../services/api";
 
 type User = {
@@ -25,9 +26,35 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData); //Cria o context com as tipagens
 
+export function signOut() {
+  //se acontecer um erro que nao foi de refreshToken, se user deletado, nos cookies token invalido etc, desligar o user
+  //iremos apagar os cookies e redirecionar para home
+  destroyCookie(undefined, "nextauth.token");
+  destroyCookie(undefined, "nextauth.refreshToken");
+  Router.push("/");
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user; // !!user == undefined = false, se nao true
+
+  //sempre que carregar a app iremos buscar as informações, permissions, roles, etc no back-end, nao iremos guardar no localStorage
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies(); //nookies obtém todos os cookies criados no storage, nesse caso coloca {nextauth.token} na var token
+
+    if (token) {
+      //fazer o pedido das informações do usuário (AuthToken está automaticamente adicionado em todas requisições na api)
+      api
+        .get("/me")
+        .then((response) => {
+          const { email, permissions, roles } = response.data;
+          setUser({ email, permissions, roles }); //atualizamos info do user
+        })
+        .catch(() => {
+          signOut();
+        });
+    }
+  }, []);
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
@@ -42,9 +69,25 @@ export function AuthProvider({ children }: AuthProviderProps) {
       //localStorage - so existe cliente side, nao existe no servidor e nem pode ser acessado pelo servidor
       //cookies - armazenar cliente e pode ser acessado no cliente e no servidor (vamos user este!)
 
+      //1 param: undefined = sempre tratar pelo lado do browser, add, rem, etc, tem que ser undefined
+      //2 param: nome do cookie, ex: nextauth.token
+      //3 param: valor do cookie, ex: token
+      //4 param: {} informações adicionais do cookie
+      setCookie(undefined, "nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30, //30 dias - responsabilidade do back-end verificar expiração do token e realizar acoes necessárias
+        path: "/",
+      });
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: "/",
+      });
+
       setUser({ email, permissions, roles }); //salvar as informações do usuário
 
-      Router.push("/dashboard"); //redirecionar user para uma página, também há o {useRouter} - faz mesmo
+      //precisamos adicionar o token ao headers, antes de redirecionar user, se nao sempre irá fazer uma requisição com Authorization undefined para o back-end
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+      Router.push("/dashboard "); //redirecionar user para uma página, também há o {useRouter} - faz mesmo
     } catch (error) {
       console.log(error);
     }
