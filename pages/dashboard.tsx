7d849f56d56c15bd9da9ab813a09738540578d63
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-08-31 22:41:42 +0200

    UP verify user data, refresh and signOut

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,19 @@
 import type { NextPage } from "next";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { api } from "../services/api";
 import styles from "../styles/Home.module.css";
 
 const Dashboard: NextPage = () => {
-  const { user } = useContext(AuthContext);
+  const { user, isAuthenticated } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (isAuthenticated) return;
+    api
+      .get("/me")
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error));
+  }, [isAuthenticated]);
   return (
     <div className={styles.container}>
       <h1>Dashboard: {user?.email}</h1>
