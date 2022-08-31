8cda3dda03b7970539bf45bbb1cc537d0fe991c9
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-08-31 10:23:15 +0200

    UP add type user, e setUser to context

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,12 @@
-import { createContext, ReactNode } from "react";
+import Router from "next/router";
+import { createContext, ReactNode, useState } from "react";
 import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 
 type SignInCredentials = {
   email: string;
@@ -9,6 +16,7 @@ type SignInCredentials = {
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>; //vai receber as credenciais do usuário
   isAuthenticated: boolean;
+  user: User | undefined;
 };
 
 type AuthProviderProps = {
@@ -18,7 +26,8 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData); //Cria o context com as tipagens
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user; // !!user == undefined = false, se nao true
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
@@ -26,14 +35,23 @@ export function AuthProvider({ children }: AuthProviderProps) {
         email,
         password,
       });
-      console.log(response.data);
+
+      const { token, refreshToken, permissions, roles } = response.data; //receber permissões, roles da server api
+
+      //sessionStorage - ao sai da aplicação é apagado, nao existe mais
+      //localStorage - so existe cliente side, nao existe no servidor e nem pode ser acessado pelo servidor
+      //cookies - armazenar cliente e pode ser acessado no cliente e no servidor (vamos user este!)
+
+      setUser({ email, permissions, roles }); //salvar as informações do usuário
+
+      Router.push("/dashboard"); //redirecionar user para uma página, também há o {useRouter} - faz mesmo
     } catch (error) {
       console.log(error);
     }
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
