b6e153ceecb6eca60304c7e45d829aba95959be5
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-01 15:56:54 +0200

    ADD validate user auth, redirect home

---
+++ utils/withSSRAuth.ts
@@ -0,0 +1,36 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies } from "nookies";
+
+//<P> é uma tipagem, que foi definida anteriormente na função original e esta sendo passada para as outras funções
+
+//Vamos usar uma HighOrderFunction recebe uma função e retorna uma função
+export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+  //definimos que withSSRGuest recebe tipo <P> definido na função original que chamou - retornará uma ": GetServerSideProps"
+  //recebemos uma função na var "fn" tipo GetServerSideProps que recebe tipo <P> definido na função original que chamou
+
+  //retornamos uma função tipo Promise<GetServerSidePropsResult<P>> que recebe <P> da função original que chamou
+  //que tem param "ctx" tipo GetServerSidePropsContext
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    //Com parse cookies podemos obter os cookies, em array, temos que usar var: ctx, outras como req ou req.cookies, ctx.req.cookies nao funcionam no parse
+    const cookies = parseCookies(ctx);
+
+    //verificamos se "NAO" existe token, e vamos redirecionar para o / - home - login
+    if (!cookies["nextauth.token"]) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false, //falar se vai sempre acontecer o redirecionamento ou só dessa ver por uma condição
+        },
+      };
+    }
+
+    //caso nao existir o cookie retornamos a função original/recebemos como parâmetro
+    return await fn(ctx);
+  };
+}
