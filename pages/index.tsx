f79feb2e160bfd7ccd7f671825980c107cefeb5c
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-01 14:50:16 +0200

    UP add GSS and user redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,9 @@
-import type { NextPage } from "next";
+import type {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  NextPage,
+} from "next";
+import { parseCookies } from "nookies";
 import { FormEvent, useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import styles from "../styles/Home.module.css";
@@ -32,6 +37,28 @@ const Home: NextPage = () => {
       <button type="submit">Entrar</button>
     </form>
   );
+};
+
+//com cookies podem ter acesso aos cookies pelos dois ambientes, client and server
+//há nao ser se for HTTPOnly cookies então é somente pelo server.
+
+//Vamos verificar os cookies pelo lado servidor para redirecionar o user
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+) => {
+  //Com parse cookies podemos obter os cookies, em array, temos que usar var: ctx, outras como req ou req.cookies, ctx.req.cookies nao funcionam no parse
+  const cookies = parseCookies(ctx);
+
+  //verificamos se existe, e vamos redirecionar para o /dashboard
+  if (cookies["nextauth.token"]) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false, //falar se vai sempre acontecer o redirecionamento ou só dessa ver por uma condição
+      },
+    };
+  }
+  return { props: {} };
 };
 
 export default Home;
